a4d3f588d3419dfd1a93249b9bb381daa93e13d6
ns <devnull@localhost>
2010-08-06 23:55:43 -0400

    minor additions

--- Tests.py
+++ Tests.py
@@ -16,11 +16,12 @@ def makeGST(unique):
     jacksink.set_property('connect', 'none')
     player.set_property("video-sink",  fakesink)
     player.set_property("audio-sink",  jacksink)
-    filename = './testNoise.mp3'
+    basename = os.path.abspath('.')
+    filename = basename+'/testNoise.mp3'
     if not os.path.exists(filename): raise IOError
     player.set_property("uri", "file://" + filename)
     player.set_state(gst.STATE_PAUSED)
-    return True
+    return player
 
 
 def makeJack(unique):
@@ -30,13 +31,30 @@ def makeJack(unique):
     jack.activate()
     systemPorts = set(jack.get_ports())
     if not selfName+":out_"+unique+"_1" in jack.get_ports(): return False
-    print [input for input in systemPorts if ":playback" in input]
-    
+    return True
+
+def makeConnect(unique):
+    allPorts = jack.get_ports()
+    selfPort = [port for port in allPorts if unique in port][0]
+    inPorts = [port for port in allPorts if 'playback' in port]
+    try:
+        for port in inPorts:
+            jack.connect(selfPort, port)
+    except:
+        return False
+    return True
+
+def makeGo(player):
+    player.set_state(gst.STATE_PLAYING)
+    time.sleep(5)
     return True
 
 def runTest():
     unique = str(time.time())
-    makeGST(unique)
-    makeJack(unique)
+    player = makeGST(unique)
+    print "GST Status: ", bool(player)
+    print "Jack Status: ", makeJack(unique)
+    print "Connect Status: ", makeConnect(unique)
+    print "Go status: ", makeGo(player)
 
 runTest()
